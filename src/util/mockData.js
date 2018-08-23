4397ccc33e86e8e3574cdb55f792e1525d28e96a
Jonathanwmaddison <jonathanwmaddison@gmail.com>
2018-08-23 11:15:11 -0400

    initial expo app

---
+++ src/util/mockData.js
@@ -0,0 +1,218 @@
+const mockData = [
+  {
+    "transId": 24,
+    "custId": 0,
+    "transTime": 1515703912733,
+    "transAmt": -53.23,
+    "description": "Team Lunch",
+    "transTo": "Restaurant",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 23,
+    "custId": 0,
+    "transTime": 1515703912113,
+    "transAmt": -53.23,
+    "description": "Team Lunch",
+    "transTo": "Restaurant",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 22,
+    "custId": 0,
+    "transTime": 1515703911630,
+    "transAmt": -53.23,
+    "description": "Team Lunch",
+    "transTo": "Restaurant",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 21,
+    "custId": 0,
+    "transTime": 1515703910985,
+    "transAmt": -53.23,
+    "description": "Team Lunch",
+    "transTo": "Restaurant",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 20,
+    "custId": 0,
+    "transTime": 1515703910265,
+    "transAmt": -53.23,
+    "description": "Team Lunch",
+    "transTo": "Restaurant",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 19,
+    "custId": 0,
+    "transTime": 1515703909606,
+    "transAmt": -53.23,
+    "description": "Team Lunch",
+    "transTo": "Restaurant",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 18,
+    "custId": 0,
+    "transTime": 1515703909120,
+    "transAmt": -53.23,
+    "description": "Team Lunch",
+    "transTo": "Restaurant",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 17,
+    "custId": 0,
+    "transTime": 1515703868742,
+    "transAmt": 621.23,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 16,
+    "custId": 0,
+    "transTime": 1515703868006,
+    "transAmt": 621.23,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 15,
+    "custId": 0,
+    "transTime": 1515703866928,
+    "transAmt": 621.23,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 14,
+    "custId": 0,
+    "transTime": 1515703018512,
+    "transAmt": 621.23,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 13,
+    "custId": 0,
+    "transTime": 1515703016968,
+    "transAmt": 621.23,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 12,
+    "custId": 0,
+    "transTime": 1515703011771,
+    "transAmt": 621.23,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 11,
+    "custId": 0,
+    "transTime": 1515702996305,
+    "transAmt": 421,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 10,
+    "custId": 0,
+    "transTime": 1515702985680,
+    "transAmt": 20000,
+    "description": "Profit",
+    "transTo": "Checking Account",
+    "transFrom": "Customers"
+  },
+  {
+    "transId": 9,
+    "custId": 0,
+    "transTime": 1515702966650,
+    "transAmt": -20000,
+    "description": "Savings",
+    "transTo": "Savings Account",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 8,
+    "custId": 0,
+    "transTime": 1515702798901,
+    "transAmt": -33000.13,
+    "description": "Salaries",
+    "transTo": "Employees",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 7,
+    "custId": 0,
+    "transTime": 1515702781539,
+    "transAmt": -1000.13,
+    "description": "Macbook air pro",
+    "transTo": "Apple",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 6,
+    "custId": 0,
+    "transTime": 1515701468425,
+    "transAmt": -3232.13,
+    "description": "Macbook pro",
+    "transTo": "Apple",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 5,
+    "custId": 0,
+    "transTime": 1515701467522,
+    "transAmt": -3232.13,
+    "description": "Macbook pro",
+    "transTo": "Apple",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 4,
+    "custId": 0,
+    "transTime": 1515701461996,
+    "transAmt": -3000,
+    "description": "Macbook pro",
+    "transTo": "Apple",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 3,
+    "custId": 0,
+    "transTime": 1515701443154,
+    "transAmt": -2000,
+    "description": "Hamilton tickets",
+    "transTo": "Lin Manuel Miranda",
+    "transFrom": "Checking Account"
+  },
+  {
+    "transId": 2,
+    "custId": 0,
+    "transTime": 1515701406019,
+    "transAmt": 500000,
+    "description": "VC money",
+    "transTo": "Checking Account",
+    "transFrom": "VC 2"
+  },
+  {
+    "transId": 1,
+    "custId": 0,
+    "transTime": 1515701357307,
+    "transAmt": 50000,
+    "description": "Initial deposit",
+    "transTo": "Checking Account",
+    "transFrom": "New VC"
+  }
+];
