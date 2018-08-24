255c6bb8db843bd78db6d13d2c1e76ee03de28b4
Jonathanwmaddison <jonathanwmaddison@gmail.com>
2018-08-24 11:39:18 -0400

    add icons to sorting buttons

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { func, bool, string, number } from 'prop-types';
+import { View, Text, FlatList } from 'react-native';
 import _ from 'lodash';
 import { SearchBar, ButtonGroup } from 'react-native-elements';
 import TransactionList from './TransactionList';
+import { FontAwesome } from '@expo/vector-icons';
+
+const propTypes = {
+  handleSortPress: func.isRequired,
+  handleSearchInput: func.isRequired,
+  ascendingSort: bool.isRequired,
+  sortBy: number.isRequired,
+  search: string,
+};
+const defaultProps = {
+  search: null,
+};
 
 const sortKeys = {
   0: 'transAmt',
   1: 'transTime',
 };
 
-const Dashboard = ({ handleSortPress, handleSearchInput, sortBy, search }) => (
-  <View style={{ flex: 1}}>
+export const sortButtons = (ascendingSort, sortBy ) => {
+  const iconName = ascendingSort ? 'sort-up' : 'sort-down';
+  return [
+    {
+      element: () => (
+        <Text>
+          {' '}
+          Amount {sortBy === 0 && <FontAwesome name={iconName} size={20} />}
+        </Text>
+      ),
+    },
+    {
+      element: () => (
+        <Text>
+          {' '}
+          Date {sortBy === 1 && <FontAwesome name={iconName} size={20} />}
+        </Text>
+      ),
+    },
+
+  ];
+};
+
+const Dashboard = ({
+  handleSortPress,
+  handleSearchInput,
+  ascendingSort,
+  sortBy,
+  search,
+}) => (
+  <View style={{ flex: 1, paddingBottom: 15 }}>
     <SearchBar
       onChangeText={handleSearchInput}
       placeholder="Search your transactions..."
@@ -19,12 +61,19 @@ const Dashboard = ({ handleSortPress, handleSearchInput, sortBy, search }) => (
     <ButtonGroup
       onPress={handleSortPress}
       selectedIndex={sortBy}
-      buttons={['Sort Amount', 'Sort Date']}
+      buttons={sortButtons(ascendingSort, sortBy)}
       containerStyle={{ height: 30, marginTop: 20 }}
       selectedTextStyle={{ fontWeight: 'bold' }}
     />
-    <TransactionList sortBy={sortKeys[sortBy]} search={search} />
+    <TransactionList
+      ascendingSort={ascendingSort}
+      sortByKey={sortKeys[sortBy]}
+      search={search}
+    />
   </View>
 );
+
+Dashboard.propTypes = propTypes;
+Dashboard.defaultProps = defaultProps;
 
 export default Dashboard;
