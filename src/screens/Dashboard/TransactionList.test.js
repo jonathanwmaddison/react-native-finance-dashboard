2b8da0e31380100c5a31b38ff2db175c2c1d8833
Jonathanwmaddison <jonathanwmaddison@gmail.com>
2018-08-24 00:38:46 -0400

    add a test for Dashboard

---
+++ src/screens/Dashboard/TransactionList.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer'
+import { mockTransactions } from '../../util/mockData';
+import { TransactionList } from './TransactionList'
+
+jest.mock('./TransactionItem', () => 'TransactionItem')
+
+describe('TransactionList', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = renderer
+      .create(<TransactionList transactions={mockTransactions} />)
+  });
+  it('[snapshot] matches', () => {
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+  it('[method] keyExtractor gets correct key', () => {
+    expect(wrapper.getInstance().keyExtractor(mockTransactions[0])).toBe('24')
+  })
+})
